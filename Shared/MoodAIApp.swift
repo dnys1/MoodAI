c439d5f65d42cc9488ce170faa5fdc724ee86612
Dillon Nys <dnys1@asu.edu>
2020-08-22 11:01:41 -0700

    Initial Commit

---
+++ Shared/MoodAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodAIApp.swift
+//  Shared
+//
+//  Created by Dillon Nys on 8/22/20.
+//
+
+import SwiftUI
+
+@main
+struct MoodAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
